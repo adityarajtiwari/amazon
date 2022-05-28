f7248a0a7bc45431be98083fd31bfb97b4948bef
adityarajtiwari <adityarajtiwari166@gmail.com>
2022-05-28 13:53:48 +0530

    3rd commit

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,3 +1,13 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+// import auth from'firebase/compat/auth';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAHM36wwNDoJK3JwdO3fHXR6JYZRbrSUEw",
@@ -8,3 +18,9 @@ const firebaseConfig = {
     appId: "1:966112023982:web:63eb03a8de70d5c6f0a43f",
     measurementId: "G-Y2NE03V5S2"
   };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+
+export { db};
